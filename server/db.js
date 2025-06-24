6595df4de19a53704b2f4fd0c997450aaf71e495
EvanCzako <evan.czako@gmail.com>
2025-06-24 00:04:45 -0400

    Update db for Render

--- server/db.js
+++ server/db.js
@@ -1,9 +1,15 @@
 import sqlite3 from 'sqlite3';
 import { open } from 'sqlite';
+import path from 'path';
 
 export async function getDB() {
+    const isRender = process.env.RENDER === 'true'; // Render sets this automatically
+    const dbPath = isRender
+        ? '/data/data.db' // <-- mounted persistent disk on Render
+        : path.resolve('./data.db'); // <-- local dev
+
     return open({
-        filename: './data.db',
+        filename: dbPath,
         driver: sqlite3.Database,
     });
 }
